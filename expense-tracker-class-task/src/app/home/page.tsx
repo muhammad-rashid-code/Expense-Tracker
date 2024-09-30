fe8fad80caf71174940e08677f9feeda794120e5
muhammad rashid <m.rashid212823@gmail.com>
2024-09-30 05:20:41 +0500

    home logout btn

--- expense-tracker-class-task/src/app/home/page.tsx
+++ expense-tracker-class-task/src/app/home/page.tsx
@@ -10,28 +10,28 @@ export default function Home() {
   return (
     <>
       {/* {user ? (
-        <h1>
-          Welcome, <span>{user.email.split("@")[0]}</span>!
-        </h1>
-      ) : (
-        <h1>Please log in.</h1>
-      )} */}
+<h1>
+Welcome, <span>{user.email.split("@")[0]}</span>!
+</h1>
+) : (
+<h1>Please log in.</h1>
+)} */}
       {user ? (
         <h1>
           Welcome,{" "}
           <span style={{ backgroundColor: "rgb(225 229 233)" }}>
             {user.email?.split("@")[0] || "User"}
-          </span>
+          </span>{" "}
+          <ButtonComp
+            btnLabel={"Sign Out"}
+            btnHandler={() => {
+              serviceSignOut();
+            }}
+          />
         </h1>
       ) : (
         <></>
-      )}{" "}
-      <ButtonComp
-        btnLabel={"Sign Out"}
-        btnHandler={() => {
-          serviceSignOut();
-        }}
-      />
+      )}
     </>
   );
 }
